c7e29c628f559ab0752f83050deb4b65dfb07e0c
DerickMacbookAir <derickdev6@gmail.com>
2022-05-05 08:02:03 -0500

    connection updated and working

--- connection.py
+++ connection.py
@@ -1,30 +1,32 @@
-import sqlite3
+import pymongo
 
 
 def main():
     try:
-        sqliteConnection = sqlite3.connect('hwood.db')
-        cursor = sqliteConnection.cursor()
-        print("Database created and Successfully Connected to SQLite")
+        # Connecting to server
+        CONNECTION_STRING = "mongodb://localhost:27017"
+        client = pymongo.MongoClient(CONNECTION_STRING)
 
-        sqlite_select_query = """SELECT * FROM Movies m """
-        cursor.execute(sqlite_select_query)
-        records = cursor.fetchall()
-        print("Total rows are:  ", len(records))
+        # Selecting db
+        mydb = client['SismosAntioquia']
+
+        # Selecting collection
+        col = mydb['Sismos']
+
+        records = list(col.find())
         # print("Printing each row")
         # for row in records:
         #     print("Name: ", row[0])
         #     print("Title: ", row[1])
         #     print("\n")
-        print(records[0][1])
+        print(f"Items = {len(records)}")
+        for row in records:
+            print(row)
 
-        cursor.close()
-    except sqlite3.Error as exception:
-        print("Database not created and Failed to Connect" + exception)
+    except Exception as e:
+        print("Database not created and Failed to Connect")
     finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("The SQLite connection is closed")
+        print("Program ended successfully")
 
 
 if __name__ == '__main__':
